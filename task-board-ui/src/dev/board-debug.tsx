db3da8e0e43c9610def624ffdeb07f1c9540d977
Adrian-Oz <adrianozorekv1@gmail.com>
2026-02-21 15:11:59 +0100

    Delete Issue

--- task-board-ui/src/dev/board-debug.tsx
+++ task-board-ui/src/dev/board-debug.tsx
@@ -4,14 +4,11 @@ import { useBoardStore } from "@/store/board-store";
 export function BoardDebug() {
   const addColumn = useBoardStore((s) => s.addColumn);
   const addIssue = useBoardStore((s) => s.addIssue);
-
-  let number = 1;
-
   return (
     <div className="flex gap-2">
       <Button
         onClick={() => {
-          addIssue({ title: "KURWA" });
+          addIssue({ title: "DEBUG TITLLE" });
         }}
       >
         Add Issue
